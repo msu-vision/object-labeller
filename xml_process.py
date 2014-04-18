57790c3b5a1246ae7783cc696e27266d9360c7e6
Vlad Shakhuro <shahurik@ya.ru>
2014-04-18 11:13:16 +0400

    Add xml processing script

---
+++ xml_process.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from sys import argv, stdout, exit
+from os.path import dirname
+from xml.etree.ElementTree import parse
+
+if len(argv) != 2:
+    stdout.write('Usage: %s labelling_path' % argv[0])
+    exit(1)
+
+labelling_path = argv[1]
+
+batch_tag = parse(labelling_path).getroot()
+
+for photo_tag in batch_tag.findall('Photo'):
+    bboxes = photo_tag.find('BBoxes')
+    if bboxes is None:
+        bboxes = []
+    else:
+        bboxes = bboxes.findall('BBox')
+
+    for bbox_tag in bboxes:
+        bbox_id = int(bbox_tag.attrib['ID'])
+        x = int(bbox_tag.find('X').text)
+        y = int(bbox_tag.find('Y').text)
+        w = int(bbox_tag.find('W').text)
+        h = int(bbox_tag.find('H').text)
+
+        stdout.write('%s %d %d %d %d %d\n' % (photo_tag.find('File').text, x, y, w, h, bbox_id))
